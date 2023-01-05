4c3444341a0d7826f5a1a23cef7f39fd9341aba1
Homayoun <en.masoomi@gmail.com>
2023-01-05 16:10:04 +0330

    k median

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,22 +11,38 @@ public class Main {
 
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int result = solve(n);
+        long[] arr = new long[n];
+        arr[0] = in.nextLong();
+        for(int i=1; i<n; i++) {
+            int input = in.nextInt();
+            insetSorted(i, input, arr);
+        }
+        String result = solve(n, arr);
         System.out.println(result);
+
     }
 
-    public int solve(int n) {
-
-        int count = 0;
-        for (int a = 1;  a < n; a++) {
-            for(int b=a; b <=n-a ; b++) {
-                int c = n - a - b;
-                if(a + b > c && c >=b) {
-                    count++;
-                }
-            }
+    private void insetSorted(int index, long input, long[] arr) {
+        int i=index;
+        while( i>0 && arr[i-1] > input) {
+            arr[i] = arr[i-1];
+            i--;
         }
+        arr[i] = input;
+    }
 
-        return count;
+    public String solve(int n ,long[] arr) {
+
+        long M = 0;
+        if(n % 2 ==0) {
+            M = arr[(n / 2)-1];
+        } else {
+            M = arr[((n+1) / 2)-1];
+        }
+        long cost = 0;
+        for(int i=0; i<n; i++)
+            cost += Math.abs(arr[i]-M);
+
+        return M + " " + cost;
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,20 +1,23 @@
-import org.junit.Before;
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SolutionTest {
 
-    private Main solution;
+    private static Main solution;
 
-    @Before
-    public void setUp() {
+    @BeforeAll
+    public static void setUp() {
         solution = new Main();
     }
 
     @Test
+    @DisplayName("test solution")
     public void test() {
-        assertEquals(1, solution.solve(5));
-        assertEquals(3, solution.solve(12));
+        assertEquals("3 4", solution.solve(2, new long[]{3, 7}));
+        assertEquals("3 6", solution.solve(5, new long[]{1, 2, 3, 4, 5}));
     }
 }
