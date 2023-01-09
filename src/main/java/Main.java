11e0687f85065dca6ea0429063eb3ac24adc506d
Homayoun <en.masoomi@gmail.com>
2023-01-09 18:06:59 +0330

    selection sort

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,24 +10,34 @@ public class Main {
     public void readAndSolve() {
 
         Scanner in = new Scanner(System.in);
-        long n = in.nextLong();
-        long result = solve(n);
-        System.out.println(result);
-
-    }
-
-    public long solve(long n) {
-
-        long triangleCount = 0;
-        for(int a = 1; a <= n/3; a++) {
-            triangleCount += ((n-(3*a))/2) - (max(0, ((n/2)-(2*a)+1))) + 1;
+        int n = in.nextInt();
+        long[] arr = new long[n];
+        for(int i=0; i<n; i++) {
+            arr[i] = in.nextLong();
         }
-        return triangleCount;
+        long[] result = solve(n, arr);
+        for(int i=0; i<n; i++) {
+            System.out.print(result[i] + " ");
+        }
     }
 
-    private long max(long a, long b) {
-        if(a >= b)
-            return a;
-        return b;
+    public long[] solve(int n, long[] arr) {
+
+        for(int i=0; i<n; i++) {
+            int minIndex = i;
+            for(int j = i; j<n; j++) {
+                if(arr[j] < arr[minIndex])
+                    minIndex = j;
+            }
+            swap(arr, minIndex, i);
+        }
+        return arr;
     }
+
+    public void swap(long[] arr, int minIndex, int i) {
+        long temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class SolutionTest {
 
@@ -17,7 +17,7 @@ public class SolutionTest {
     @Test
     @DisplayName("test solution")
     public void test() {
-        assertEquals(3, solution.solve(12));
-        assertEquals(126841016, solution.solve(78028));
+        assertArrayEquals(new long[]{1, 2, 2, 2, 3, 4, 5, 7, 9, 12}, solution.solve(10, new long[]{2, 1, 7, 12, 3, 4, 5, 2, 2, 9}));
+        assertArrayEquals(new long[]{1, 2, 3}, solution.solve(3, new long[]{3, 2, 1}));
     }
 }
