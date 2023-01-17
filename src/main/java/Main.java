5fa881f17105e9b681396a5bcb650cf96de445fe
Homayoun <en.masoomi@gmail.com>
2023-01-17 14:41:01 +0330

    set partition

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,29 +12,51 @@ public class Main {
 
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        long[] arr = new long[n];
+        int k = in.nextInt();
+        int[] arr = new int[n];
         for(int i=0; i<n; i++)
-            arr[i] = in.nextLong();
+            arr[i] = in.nextInt();
 
-        System.out.println(solve(n, arr));
+        System.out.println(solve(n, k, arr));
+        in.close();
     }
 
-    public long solve(int n, long[] arr) {
+    public int solve(int n, int k, int[] arr) {
 
-        long arrSum = Arrays.stream(arr).sum();
+        if(k == 1)
+            return Arrays.stream(arr).max().getAsInt();
 
-        long min = Long.MAX_VALUE;
-        for(int mask=1; mask< (1<<n) ; mask++) {
-            long sum = 0;
-            for(int j=0; j<n; j++) {
-                if((mask & (1<<j)) != 0)
-                   sum +=arr[j];
-            }
-            long res = Math.abs(arrSum - 2*sum);
-            if(res < min)
-                min = res;
+        if(k > 2)
+            return Arrays.stream(arr).min().getAsInt();
+
+        int min = Integer.MAX_VALUE;
+
+        for(int i=1; i< arr.length ; i++) {
+            int[] sub1 = sub(0, i, arr);
+            int[] sub2 = sub(i, arr.length, arr);
+            int tmp = min(max(sub1), max(sub2));
+            if(tmp < min)
+                min = tmp;
         }
        return min;
+    }
+
+    public int[] sub(int start, int end, int[] arr) {
+        int[] res = new int[end-start];
+        for(int i=0; end > start; i++, start++) {
+            res[i] = arr[start];
+        }
+        return res;
+    }
+
+    public int max(int[] arr) {
+        return Arrays.stream(arr).max().getAsInt();
+    }
+
+    public int min (int a, int b) {
+        if(a<b)
+            return a;
+        return b;
     }
 
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -18,8 +18,11 @@ public class SolutionTest {
     @Test
     @DisplayName("test solution")
     public void test() {
-//        assertEquals( 3, solution.solve(18, 15));
-//        assertEquals( 1, solution.solve(3, 11));
-//        assertEquals( 7, solution.solve(14, 21));
+        assertEquals( 3, solution.solve(3, 2, new int[]{3, 2, 3}));
+        assertEquals( 2, solution.solve(6, 2, new int[]{3, 2, 3, 1, 2,2}));
+        assertEquals( 1, solution.solve(10, 2, new int[]{1, 2, 3,4,4,4,4,1,2,3}));
+        assertEquals( 1, solution.solve(5, 2, new int[]{3, 2, 3, 1, 1}));
+        assertEquals( 2, solution.solve(5, 3, new int[]{5, 4, 3, 2, 2}));
+        assertEquals( 4, solution.solve(4, 1, new int[]{1, 3, 4, 2}));
     }
 }
