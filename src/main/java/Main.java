8f7ecb965f628fb4706af63ebf6891319e3e3a6a
Mohammad Masoomi <en.masoomi@gmail.com>
2023-02-12 23:16:52 +0330

    sales man

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,69 +1,168 @@
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.lang.Math.max;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Main solution = new Main();
         solution.readAndSolve();
     }
 
-    public void readAndSolve() {
+    public void readAndSolve() throws FileNotFoundException {
 
-        Scanner in = new Scanner(System.in);
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("input10.txt");
+        Scanner in = new Scanner(inputStream);
         int n = in.nextInt();
-        int m = in.nextInt();
+        int s = in.nextInt();
 
-        long[][] arr = new long[n][m];
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                arr[i][j] = in.nextLong();
-            }
-        }
-        System.out.println(solve(n, m, arr));
-    }
-
-    public long solve(int n, int m, long[][] arr) {
-
-        long max = Long.MAX_VALUE;
-        long[][][] dp = new long[n][m][2];
+        Input[] inputs = new Input[n+1];
 
         for(int i=0; i<n; i++) {
-            for(int j=m-1; j>=0; j--) {
-                if(i==0 && j==0) {
-                    dp[i][j][0] = arr[i][j];
-                } else if(i==0) {
-                    dp[i][j][0] = max;
-                } else if(j==0) {
-                    dp[i][j][0] = min(dp[i-1][j][1], dp[i-1][j][0],dp[i][j+1][0]) + arr[i][j];
-                } else if(j+1==m) {
-                    dp[i][j][0] = min(dp[i-1][j][1], dp[i-1][j][0],max) + arr[i][j];
-                } else
-                    dp[i][j][0] = min(dp[i - 1][j][1], dp[i - 1][j][0], dp[i][j + 1][0]) + arr[i][j];
+            int length = in.nextInt();
+            List<Integer> weights = new ArrayList<>();
+            for(int j=0; j<length; j++) {
+                weights.add(in.nextInt());
             }
-            for(int j=0; j<m; j++) {
-                if(i==0 && j==0) {
-                    dp[i][j][1] = arr[i][j];
-                } else if(i==0) {
-                    dp[i][j][1] = dp[i][j-1][1] + arr[i][j];
-                } else if(j==0) {
-                    dp[i][j][1] = min(dp[i-1][j][1], dp[i-1][j][0],max) + arr[i][j];
-                } else if(j+1==m) {
-                    dp[i][j][1] = min(dp[i-1][j][1], dp[i-1][j][0],dp[i][j-1][1]) + arr[i][j];
-                } else
-                    dp[i][j][1] = min(dp[i - 1][j][1], dp[i - 1][j][0], dp[i][j - 1][1]) + arr[i][j];
-            }
+            inputs[i] = makeInput(length, weights);
         }
-
-        return dp[n-1][m-1][1];
+        inputs[n] = null;
+        System.out.println(solve(n, s, inputs));
     }
 
-    private long min(long a, long b, long c) {
-        if(a <= b && a <= c) {
-            return a;
-        } else if(b<= a && b <=c) {
-            return b;
+    public String solve(int n, int s, Input[] inputs) {
+
+        int[][] dp = new int[n+1][s+1];
+        String[][] par = new String[n+1][s+1];
+        int[] w = new int[n+1];
+
+        for(int j=0; j<=n; j++) {
+            w[j] = calculateWeight(s, inputs[j]);
         }
-        return c;
+        for(int j=0; j<s; j++) {
+            dp[0][j] = 0;
+        }
+        for(int i=1; i<=n; i++) {
+            int count = catchableCount(w[i-1], inputs[i-1]);
+            for(int j=0; j<=s; j++) {
+                dp[i][j] = dp[i - 1][j];
+                par[i][j] = "0";
+                if(j - w[i-1] >= 0 && dp[i - 1][j - w[i-1]] + count > dp[i - 1][j]) {
+                    dp[i][j] = dp[i - 1][j - w[i-1]] + count;
+                    par[i][j] = (count == inputs[i-1].length && inputs[i-1].length > 1) ? "2" : "1";
+                }
+            }
+
+        }
+
+        StringBuilder result = new StringBuilder();
+        int remainS = s;
+        for(int i=n; i>0; i--) {
+
+            if(!par[i][remainS].equalsIgnoreCase("0")) {
+                result.insert(0, par[i][remainS]);
+                remainS -= w[i-1];
+            } else {
+                result.insert(0, par[i][remainS]);
+            }
+        }
+
+        System.out.println(dp[n][s]);
+        return result.toString();
+    }
+
+    private int calculateWeight(int s, Input input) {
+
+        if(input == null)
+            return 0;
+
+        if(s >= input.sum())
+            return input.sum();
+
+        if(s >= input.min())
+            return input.min();
+
+        return 0;
+    }
+
+    private int catchableWeight(int s, Input input) {
+        if(s >= input.sum())
+            return input.sum();
+
+        if(s >= input.min())
+            return input.min();
+
+        return 0;
+    }
+
+    private int catchableCount(int s, Input input) {
+
+        if(input == null)
+            return 0;
+
+        if(s >= input.sum())
+            return input.length;
+
+        if(s >= input.min())
+            return 1;
+
+        return 0;
+    }
+
+    private String calculateCost(int s, Input input) {
+
+        if(s >= input.sum())
+            return "2";
+
+        if(s >= input.min())
+            return "1";
+
+        return "0";
+    }
+
+    class Input {
+        int length = 0;
+        List<Integer> list = new ArrayList<>();
+
+        public int sum() {
+            return this.list.stream().mapToInt(i -> i.intValue()).sum();
+        }
+
+        public int min() {
+            return this.list.stream().mapToInt(i ->i.intValue()).min().getAsInt();
+        }
+
+
+        public Input(int length, List<Integer> list) {
+            this.length = length;
+            this.list = list;
+        }
+
+        public int getLength() {
+            return length;
+        }
+
+        public void setLength(int length) {
+            this.length = length;
+        }
+
+        public List<Integer> getList() {
+            return list;
+        }
+
+        public void setList(List<Integer> list) {
+            this.list = list;
+        }
+    }
+
+    public Input makeInput(int i, List<Integer> integers) {
+        return new Input(i, integers);
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -3,6 +3,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -18,9 +23,39 @@ public class SolutionTest {
     @Test
     @DisplayName("test solution")
     public void test() {
-        long max = Long.MAX_VALUE;
-        assertEquals( 7, solution.solve(4, 4, new long[][]{{1,1,9,1},{9,1,9,9},{1,1,1,9},{1,1,1,1}}));
-        assertEquals( 10, solution.solve(5, 4, new long[][]{{1,1,9,1},{9,1,9,9},{1,1,1,9},{1,9,9,9},{1,1,1,1}}));
-        assertEquals( 1, solution.solve(1, 1, new long[][]{{1}}));
+        Main.Input[] inputs = new Main.Input[4];
+        inputs[0] = solution.makeInput(1, Arrays.asList(100));
+        inputs[1] = solution.makeInput(3, Arrays.asList(1,2,3));
+        inputs[2] = solution.makeInput(2, Arrays.asList(2,10));
+        inputs[3] = null;
+        assertEquals( "021", solution.solve(3, 11, inputs));
+
+        inputs = new Main.Input[4];
+        inputs[0] = solution.makeInput(3, Arrays.asList(1,1,1));
+        inputs[1] = solution.makeInput(3, Arrays.asList(1,1,1));
+        inputs[2] = solution.makeInput(3, Arrays.asList(1,1,1));
+        inputs[3] = null;
+        assertEquals( "200", solution.solve(3, 3, inputs));
+
+        inputs = new Main.Input[4];
+        inputs[0] = solution.makeInput(1, Arrays.asList(12));
+        inputs[1] = solution.makeInput(1, Arrays.asList(12));
+        inputs[2] = solution.makeInput(1, Arrays.asList(12));
+        inputs[3] = null;
+        assertEquals( "000", solution.solve(3, 11, inputs));
+
+        inputs = new Main.Input[4];
+        inputs[0] = solution.makeInput(3, Arrays.asList(1, 19, 10));
+        inputs[1] = solution.makeInput(6, Arrays.asList(1,1,1,1,1,1));
+        inputs[2] = solution.makeInput(1, Arrays.asList(12));
+        inputs[3] = null;
+        assertEquals( "020", solution.solve(3, 6, inputs));
+
+        inputs = new Main.Input[4];
+        inputs[0] = solution.makeInput(3, Arrays.asList(1, 19, 10));
+        inputs[1] = solution.makeInput(4, Arrays.asList(1,1,1,1));
+        inputs[2] = solution.makeInput(6, Arrays.asList(1,1,1,1,1,1));
+        inputs[3] = null;
+        assertEquals( "002", solution.solve(3, 6, inputs));
     }
 }
