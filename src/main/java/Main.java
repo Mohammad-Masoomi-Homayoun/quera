4a874c22c7d9e9591f9a813e0268d77b7fb685a0
Homayoun <en.masoomi@gmail.com>
2023-03-25 11:21:33 +0330

    DEV: parentheses

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,5 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
 public class Main {
 
     public static void main(String[] args) throws IOException {
@@ -17,158 +14,48 @@ public class Main {
 //        Scanner in = new Scanner(is);
 
         Scanner in = new Scanner(System.in);
-        int[][] table = new int[9][9];
-        for(int i = 0; i<9; i++)
-            for(int j = 0; j<9; j++)
-                table[i][j] = in.nextInt();
+        String input = in.nextLine();
 
-        int[][] solved = solve(table);
+        String[] solved = solve(input);
 
         if(solved == null) {
-            System.out.println("No solution exists");
+            System.out.println("-1");
             return;
         }
         OutputStreamWriter out = new OutputStreamWriter(System.out);
         BufferedWriter writer = new BufferedWriter(out);
         StringBuilder stringBuilder = new StringBuilder();
 
-        for(int i = 0; i<9; i++) {
-            for (int j = 0; j < 9; j++) {
-                stringBuilder.append(solved[i][j] + " ");
-            }
-            stringBuilder.append("\n");
+        for(int i = 0; i<solved.length; i++) {
+            stringBuilder.append(solved[i] + "\n");
         }
         writer.write(stringBuilder.toString());
         writer.flush();
     }
 
-    int[][] result;
-    boolean hasResult = false;
+    public String[] solve(String input) {
+        if(input == null || input.length() < 2 || input.startsWith(")") || input.length() % 2 != 0)
+            return null;
 
-    public int[][] solve(int[][] table) {
-
-        hasResult = false;
-        result = new int[9][9];
-        sudoku(table);
-        if(hasResult)
-            return result;
-        return null;
-    }
-
-    public void sudoku(int[][] table) {
-
-        if(hasResult)
-            return;
-
-        if (isCompleteSimple(table) /*&& isCorrect(table)*/) {
-            copyToResult(table);
-            return;
-        }
-
-        int freeIndexI = 0, freeIndexJ = 0;
-        for(int i = 0; i<9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if(table[i][j] == 0) {
-                    freeIndexI = i;
-                    freeIndexJ = j;
-                    i = 9;
-                    j = 9;
-                }
-            }
-        }
-
-        for(int k = 1; k<=9; k++) {
-            table[freeIndexI][freeIndexJ] = k;
-            if(isCorrect(table, freeIndexI, freeIndexJ)) {
-                sudoku(table);
-            }
-            table[freeIndexI][freeIndexJ] = 0;
-        }
-        return;
-    }
-
-    private void copyToResult(int[][] table) {
-        hasResult = true;
-        for(int i = 0; i<9; i++) {
-            for (int j = 0; j < 9; j++) {
-                result[i][j] = table[i][j];
-            }
-        }
-    }
-
-    private boolean isCorrect(int[][] table, int i, int j) {
-        int[] batch = new int[9];
-        for (int k = 0; k < 9; k++) {
-            batch[k] = table[i][k];
-        }
-        if(hasDuplicate(batch))
-            return false;
-
-        for (int k = 0; k < 9; k++) {
-            batch[k] = table[k][j];
-        }
-        if(hasDuplicate(batch))
-            return false;
-
-        int indexI= (i/3)*3;
-        int indexJ = (j/3)*3;
-        batch[0] = table[indexI][indexJ];
-        batch[1] = table[indexI+1][indexJ];
-        batch[2] = table[indexI+2][indexJ];
-        batch[3] = table[indexI][indexJ+1];
-        batch[4] = table[indexI+1][indexJ+1];
-        batch[5] = table[indexI+2][indexJ+1];
-        batch[6] = table[indexI][indexJ+2];
-        batch[7] = table[indexI+1][indexJ+2];
-        batch[8] = table[indexI+2][indexJ+2];
-        if(hasDuplicate(batch))
-            return false;
-
-        return true;
-    }
-
-
-    private boolean hasDuplicateSimple(int[] batch) {
-
-        Set<Integer> allBatch = new HashSet<>();
+        String[] result = new String[input.length() / 2];
+        Stack<Integer> myStack = new Stack<>();
         int counter = 0;
-        for(int i=0; i<batch.length; i++) {
-            if(batch[i] != 0) {
-                allBatch.add(batch[i]);
+
+        for(int i=0; i< input.length(); i++) {
+            if(input.charAt(i) == '(') {
+                myStack.push(i+1);
+            } else if(input.charAt(i) == ')') {
+                if(myStack.size() < 1)
+                    return null;
+
+                Integer num = myStack.pop();
+                result[counter] = num + " " + (i+1);
                 counter++;
             }
         }
-        return counter != allBatch.size();
-    }
 
-    private boolean isCompleteSimple(int[][] table) {
-        int count = 0;
-        for(int i=0; i<9; i++) {
-            for(int j=0; j<9; j++) {
-                if(table[i][j] == 0)
-                    return false;
-            }
-        }
-        return true;
-    }
-
-    private boolean hasDuplicate(int[] batch) {
-        boolean simple = hasDuplicateSimple(batch);
-       long all = Arrays.stream(batch).filter(x-> x!=0).count();
-       long distinct = Arrays.stream(batch).filter(x-> x!=0).distinct().count();
-        boolean advance = all != distinct;
-
-        if(simple != advance) {
-            System.out.println("Diff");
-            hasDuplicateSimple(batch);
-        }
-
-        return advance;
-    }
-
-    private boolean isComplete(int[][] table) {
-        return Arrays.stream(table)
-                .flatMapToInt(Arrays::stream)
-                .filter(x -> x == 0).count() == 0;
+        if(input.length() / 2 != counter)
+            return null;
+        return result;
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -3,13 +3,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class SolutionTest {
@@ -25,36 +19,9 @@ public class SolutionTest {
     @DisplayName("test solution")
     public void test() {
 
-        int[][] unsolved = new int[][]{{3, 0, 6, 5, 0, 8, 4, 0, 0},
-                {5, 2, 0, 0, 0, 0, 0, 0, 0},
-                {0, 8, 7, 0, 0, 0, 0, 3, 1},
-                {0, 0, 3, 0, 1, 0, 0, 8, 0},
-                {9, 0, 0, 8, 6, 3, 0, 0, 5},
-                {0, 5, 0, 0, 9, 0, 6, 0, 0},
-                {1, 3, 0, 0, 0, 0, 2, 5, 0},
-                {0, 0, 0, 0, 0, 0, 0, 7, 4},
-                {0, 0, 5, 2, 0, 6, 3, 0, 0}};
-        int[][] expected = new int[][]{{3, 1, 6, 5, 7, 8, 4, 9, 2},
-                {5, 2, 9, 1, 3, 4, 7, 6, 8},
-                {4, 8, 7, 6, 2, 9, 5, 3, 1},
-                {2, 6, 3, 4, 1, 5, 9, 8, 7},
-                {9, 7, 4, 8, 6, 3, 1, 2, 5},
-                {8, 5, 1, 7, 9, 2, 6, 4, 3},
-                {1, 3, 8, 9, 4, 7, 2, 5, 6},
-                {6, 9, 2, 3, 5, 1, 8, 7, 4},
-                {7, 4, 5, 2, 8, 6, 3, 1, 9}};
-        assertArrayEquals(expected, solution.solve(unsolved));
-
-        unsolved = new int[][]{{4, 0, 0, 5, 0, 2, 9, 0, 8},
-                {9, 0, 1, 0, 8, 7, 0, 0, 0},
-                {0, 8, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 4, 2, 0, 9, 8, 0, 0},
-                {0, 0, 2, 1, 5, 4, 3, 9, 0},
-                {0, 0, 9, 0, 6, 3, 0, 5, 2},
-                {2, 4, 0, 0, 0, 0, 0, 8, 9},
-                {1, 0, 0, 3, 7, 0, 0, 2, 0},
-                {3, 9, 0, 4, 0, 5, 1, 0, 0}};
-        assertEquals(null, solution.solve(unsolved));
+        String[] result = new String[]{"2 3", "5 6", "4 7", "1 8"};
+//        assertArrayEquals(result, solution.solve("(()(()))"));
+        assertNull(solution.solve("()(())((()((((()(()))(((()))(()))())((((()))()))"));
 
     }
 }
