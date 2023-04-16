45ac818cfed9e88ab70c1587ace05b7e1294f5db
Homayoun <en.masoomi@gmail.com>
2023-04-16 22:00:45 +0330

    preprocess of questions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
 
     public static void main(String[] args) throws IOException {
@@ -15,38 +16,67 @@ public class Main {
         int n = in.nextInt();
         int q = in.nextInt();
 
-        int[] arr = new int[n];
+        int[][] arr = new int[n][n];
 
         for (int i = 0; i < n; i++) {
-            arr[i] = in.nextInt();
+            arr[i][0] = i+1;
         }
 
-        int[][] questions = new int[q][2];
         for (int i = 0; i < q; i++) {
-            questions[i][0] = in.nextInt();
-            questions[i][1] = in.nextInt();
+            int command = in.nextInt();
+            if(command == 1) {
+                int a = in.nextInt() -1;
+                int b = in.nextInt() -1;
+                pushElements(arr[a], arr[b]);
+            } else if(command == 2) {
+                int c = in.nextInt() -1;
+                printLength(arr[c]);
+            } else if(command == 3) {
+                int d = in.nextInt() -1;
+                printElements(arr[d]);
+            }
         }
+    }
 
-        long[] result = solve(n, arr, q, questions);
-        Arrays.stream(result).forEach(System.out::println);
+    private void printElements(int[] d) {
+        int[] tmp = d.clone();
+        Arrays.sort(tmp);
+        tmp = Arrays.stream(tmp).filter(x->x!=0).toArray();
+        if(tmp.length == 0) {
+            System.out.println("-1");
+            return;
+        }
+        for( int i =0; i< tmp.length ; i++) {
+            System.out.print(tmp[i] + " ");
+        }
+        System.out.println();
+    }
+
+    private void printLength(int[] c) {
+        int counter = 0;
+        while (counter < c.length && c[counter] != 0) {
+            counter++;
+        }
+        System.out.println(counter);
+    }
+
+    private void pushElements(int[] a, int[] b) {
+        int counter = 0;
+        while (counter < b.length && b[counter] != 0) {
+           counter++;
+        }
+        int startIndex = counter;
+        counter = 0;
+        while (counter < a.length && a[counter] != 0) {
+            b[startIndex++] = a[counter];
+            a[counter] = 0;
+            counter++;
+        }
     }
 
     public long[] solve(int n, int[] arr, int q, int[][] questions) {
 
-        long[] sums = new long[n+1];
-        sums[0] = 0;
-        for (int i = 1; i <= n; i++) {
-            sums[i] = sums[i-1] + arr[i-1];
-        }
-
-        long[] result = new long[q];
-        for (int i = 0; i < q; i++) {
-            if(questions[i][1] == 1000000)
-                System.out.println();
-            result[i] = sums[questions[i][1]+1] - sums[questions[i][0]];
-        }
-
-        return result;
+        return new long[1];
     }
 
 
